ee22acd42169a1bb076af467412e03cfa60c4d35
Oliver DeBarros <debarros.oliver@gmail.com>
2021-03-23 18:50:11 -0500

    create extract file

---
+++ Fantasy_Soccer/fbref_extract.py
@@ -0,0 +1,12 @@
+##########################################################################################
+# File: fbref_extract.py
+# File Created: Tuesday, 23rd March 2021 6:48:13 pm
+# Author: Oliver DeBarros (debarros.oliver@gmail.com)
+# -----
+# Last Modified: Tuesday, 23rd March 2021 6:49:32 pm
+# Modified By: Oliver DeBarros (debarros.oliver@gmail.com)
+# -----
+# Description:
+#   This file stores all of the methods related to extracting data from fbref.com
+##########################################################################################
+
